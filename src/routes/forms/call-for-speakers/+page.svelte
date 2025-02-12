bc98cc42a8f2d27a7583f9e58f36db0767ca0bf3
schuster <sbraun990@gmail.com>
2025-02-11 21:01:54 -0500

    add call for speakers form

---
+++ src/routes/forms/call-for-speakers/+page.svelte
@@ -0,0 +1,9 @@
+<iframe
+	src="https://wax-warbler-fff.notion.site/ebd/194bc6eec566802e8557f2ce3ca6ac2f"
+	width="100%"
+	title="call for speakers"
+	height="1100"
+	frameborder="0"
+	allowfullscreen
+	style="border-radius: 10px; box-shadow: 5px 10px 20px 10px rgba(0,0,0,0.5);"
+></iframe>
